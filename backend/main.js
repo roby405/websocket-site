6edfb133dba2d4b63aa2ad315493456beb9541d4
roby405 <robep2341@gmail.com>
2025-04-14 03:21:49 +0300

    reverted changes

--- backend/main.js
+++ backend/main.js
@@ -35,11 +35,11 @@ app.use("/api/messages", messageRoute);
 app.use("/api", authRoute);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "dist")));
+  app.use(express.static(path.join(__dirname, "../dist/")));
 
   // Handle client-side routing
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../dist/", "index.html"));
   });
 }
 
